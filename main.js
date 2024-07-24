3a42dc0cb02e2b4c79ab6d6ac6c1faab366d4516
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2024-07-24 09:29:15 -0300

    jtu

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
-const botoes = document.querySelectorAll('.')
+const botoes = document.querySelectorAll('.parametro-senha__botao')
+console.log(botoes)
 numeroSenha.textContent = 6  
