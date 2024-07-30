d07ac292fd1f7676926fea6836a5e9ee135cc8d8
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2024-07-30 07:49:47 -0300

    uy5jku

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes = document.querySelectorAll('.parametro-senha__botao')
+
 botoes[0].onclick= diminuirTamanho
+
 let tamanhoSenha = 5 
 numeroSenha.textContent = tamanhoSenha
-    fuction diminuirTamanho() {
+
+function diminuirTamanho() {
+    tamanhoSenha = tamanhoSenha
         numeroSenha.textContent = tamanhoSenha 
     }
 
