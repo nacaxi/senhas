ccd43249afea130e465fd4d236f1eb7adcd8001a
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2024-07-30 07:41:41 -0300

    jjmh

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes = document.querySelectorAll('.parametro-senha__botao')
 console.log(botoes)
-fuction diminuirTamanho()
-console.log('teste')
+    fuction diminuirTamanho() {
+        console.log('teste')
+    }
+
 numeroSenha.textContent = 6  
