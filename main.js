9e716176b09d5754c5be4fa694cf6e725a161f17
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2024-08-07 09:34:47 -0300

    a

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes = document.querySelectorAll('.parametro-senha__botao')
+const campoSenha = document.querySelector('#campo-senha')
 
 botoes[0].onclick= diminuirTamanho
 botoes[1].onclick= aumentarTamanho
